dfc828b295cbd9f09fffdf60aa22ebfe02add3d3
VtsAshDev <vitorgguimaraes56@gmail.com>
2025-06-19 13:20:11 -0300

    Create Transaction Migrations

---
+++ database/migrations/2025_06_19_153118_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wallet_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->boolean('is_recurring')->default(false);
+            $table->string('description');
+            $table->decimal('amount');
+            $table->integer('installments')->nullable();
+            $table->date('due_date')->nullable();
+            $table->date('transaction_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
